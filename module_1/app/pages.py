23f824c791e18123015ac73d7d21a7478fc981a3
Jacob Wong <jacobwong962@gmail.com>
2025-05-24 19:41:43 -0700

    Home Page Complete

--- module_1/app/pages.py
+++ module_1/app/pages.py
@@ -4,13 +4,13 @@ bp = Blueprint("pages", __name__)
 
 @bp.route("/")
 def home():
-    return render_template("pages/home.html")
+    return render_template("pages/home.html", active_page='home')
 
 @bp.route("/projects")
 def projects():
-    return render_template("pages/projects.html")
+    return render_template("pages/projects.html", active_page='projects')
 
 @bp.route("/contact")
 def contact():
-    return render_template("pages/contact.html")
+    return render_template("pages/contact.html", active_page='contact')
 

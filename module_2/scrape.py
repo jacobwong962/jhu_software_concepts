cfd6ff8008dc31ef098cc320797c4485345d6fdb
Jacob Wong <jacobwong962@gmail.com>
2025-05-30 00:03:03 -0700

    Update scrape.py

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -1,13 +1,39 @@
 import urllib3
 from bs4 import BeautifulSoup
 
-url = "https://www.thegradcafe.com/survey/"
-http = urllib3.PoolManager()
-response = http.request("GET", url)
+def scrape_data():
+    url = "https://www.thegradcafe.com/survey/"
+    http = urllib3.PoolManager()
+    response = http.request("GET", url)
 
-soup = BeautifulSoup(response.data, 'html.parser')
-type(soup)
-rows = soup.find_all("tr")[1:]
+    soup = BeautifulSoup(response.data, 'html.parser')
 
-for row in rows[:5]:
-    cells = row.find_all('td')
+    rows = soup.find_all("tr")[1:]
+
+    scraped_data = []
+    for row in rows[:10]:
+        cells = row.find_all('td')
+        text_cells = [cell.get_text(strip=True) for cell in cells]
+        scraped_data.append(text_cells)
+    
+    return scraped_data
+
+def create_entry(scraped_data):
+    i = 0
+    entries = []
+    while i < len(scraped_data):
+        entry = {'row_1':scraped_data[i];
+                 'row_2':scraped_data[i+1]}
+        if i+2 < len(scraped_data) and len(scraped_data[i+2]) == 1:
+            entry.append(scraped_data[i+2])
+            i += 1
+        entries.append(entry)
+        i += 2
+        print(entry)
+        print('##################################')
+    return(entries)
+
+if __name__ =="__main__":
+    scraped_data = scrape_data()
+    entries = create_entry(scraped_data)
+    

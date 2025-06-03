baf1c97ee891dc94fbc3d7fa3413c4f5672fc325
Jacob Wong <jacobwong962@gmail.com>
2025-06-03 13:54:18 -0700

    Created scrape_pages()
    This new function uses the other two functions to scrape multiple pages on the website

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -1,8 +1,10 @@
 import urllib3
 from bs4 import BeautifulSoup
+import math
 
-def scrape_data():
-    url = "https://www.thegradcafe.com/survey/"
+def scrape_data(url):
+    """Scrapes raw data from cite. Any html element with the <td> tag has its
+    data stored in the list called 'scraped data.'"""
     http = urllib3.PoolManager()
     response = http.request("GET", url)
 
@@ -11,14 +13,21 @@ def scrape_data():
     rows = soup.find_all("tr")[1:]
 
     scraped_data = []
-    for row in rows[:100]:
+    for row in rows:
         cells = row.find_all('td')
         text_cells = [cell.get_text(strip=True) for cell in cells]
+        if text_cells == ['']:
+            continue
         scraped_data.append(text_cells)
     
     return scraped_data
 
 def create_entries(scraped_data):
+    """
+    Passes in the list returned from scrape_data(). Restructures this list into
+    a list of dictionaries, where each dictionary is one entry from the Grad
+    Cafe website.
+    """
     i = 0
     entries = []
     while i < len(scraped_data) - 1:
@@ -29,11 +38,35 @@ def create_entries(scraped_data):
             i += 1
         entries.append(entry)
         i += 2
-        print(entry)
-        print('##################################')
+        #print(entry)
+        #print('##################################')
     return(entries)
 
+def scrape_pages(num_entries):
+    """
+    Passes in the desired number of entries we want to scrape from the Grad
+    Cafe. Calculates the total number of pages that must be scraped. Scrapes
+    each page on the website in a for loop, and returns all of the """
+    # The gradcafe website has 20 entries per page
+    num_pages = math.ceil(num_entries/20) 
+
+    entries = []
+    for i in range(num_pages):
+        print(f'\n\n###########Page {i+1}############')
+        url = f"{base_url}?page={i}"
+        scraped_data = scrape_data(url)
+        page_entries = create_entries(scraped_data)
+        entries.extend(page_entries)
+    
+    entries = entries[0:num_entries]
+    return entries
+
 if __name__ =="__main__":
-    scraped_data = scrape_data()
-    entries = create_entries(scraped_data)
+    base_url = f"https://www.thegradcafe.com/survey/"
+    num_entries = 101
+    all_entries = scrape_pages(num_entries)
+    print(all_entries, f'\nLength of entries: {len(all_entries)}')
+
+    
+    
     

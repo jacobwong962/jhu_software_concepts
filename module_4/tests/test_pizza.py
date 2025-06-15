0429b5f4fede2e2a3df9156b55c3bcc98052422b
Jacob Wong <jacobwong962@gmail.com>
2025-06-15 15:10:25 -0700

    test files complete

--- module_4/tests/test_pizza.py
+++ module_4/tests/test_pizza.py
@@ -6,6 +6,18 @@ def example_pizza():
     return Pizza("thin", ["pesto"], "mozzarella", ["mushroom"])
 
 @pytest.mark.pizza
-def test_init():
-    assert
+def test_init(example_pizza):
+    assert isinstance(example_pizza, Pizza)
+    assert example_pizza.crust == "thin"
+    assert example_pizza.sauce == ["pesto"]
+    assert example_pizza.cheese == "mozzarella"
+    assert example_pizza.toppings == ["mushroom"]
+    assert example_pizza.cost > 0
     
+@pytest.mark.pizza
+def test_str(example_pizza):
+    assert str(example_pizza) == "Crust: thin, Sauce: ['pesto'], Cheese: mozzarella, Toppings: ['mushroom'], Cost: 11"
+
+@pytest.mark.pizza
+def test_cost(example_pizza):
+    assert example_pizza == 11

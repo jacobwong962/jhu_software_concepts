1164a50b54fc61830649a56911b940eadc094adf
Jacob Wong <jacobwong962@gmail.com>
2025-06-19 14:02:45 -0700

    Final commit v1

--- module_6/run.py
+++ module_6/run.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     #instantiates a Flask application
     my_app = app.create_app()
 
-    my_app.run(host="0.0.0.0", port=8000, debug=True)
+    my_app.run(host="0.0.0.0", port=8080, debug=True)
